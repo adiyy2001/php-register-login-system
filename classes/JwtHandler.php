b0626491c66bf06b273fc240eaafb70be148bf11
adiyy2001 <adiy@onet.pl>
2021-08-05 19:40:46 +0200

    add rest of required files

--- classes/JwtHandler.php
+++ classes/JwtHandler.php
@@ -13,31 +13,29 @@ class JwtHandler {
     protected $expire;
     protected $jwt;
 
-    public function __constructor() {
-        date_default_timezone_set('Europe/Poland');
-        $this->issuedAt + 3600;
-
+    public function __construct()
+    {
+        date_default_timezone_set('Europe/Warsaw');
+        $this->issuedAt = time();
+        
         $this->expire = $this->issuedAt + 3600;
 
-        $this->jwt_secret = "E5FEB55D546492CA5903C647BD6C73E3D04F2BA1B4CDD5517D34765C2C0E426C";
+        $this->jwt_secret = "this_is_my_secret";  
     }
 
-        public function _jwt_encode_data($iss,$data){
+    public function _jwt_encode_data($iss,$data){
 
         $this->token = array(
-            //Adding the identifier to the token (who issue the token)
             "iss" => $iss,
             "aud" => $iss,
-            // Adding the current timestamp to the token, for identifying that when the token was issued.
             "iat" => $this->issuedAt,
-            // Token expiration
             "exp" => $this->expire,
-            // Payload
             "data"=> $data
         );
 
         $this->jwt = JWT::encode($this->token, $this->jwt_secret);
         return $this->jwt;
+
     }
 
     protected function _errMsg($msg){
@@ -46,16 +44,15 @@ class JwtHandler {
             "message" => $msg
         ];
     }
-
-    public function _jwt_decode_data($jwt_token) {
-        try {
+    
+    public function _jwt_decode_data($jwt_token){
+        try{
             $decode = JWT::decode($jwt_token, $this->jwt_secret, array('HS256'));
             return [
                 "auth" => 1,
                 "data" => $decode->data
-            ]; 
-        } 
-        
+            ];
+        }
         catch(\Firebase\JWT\ExpiredException $e){
             return $this->_errMsg($e->getMessage());
         }
@@ -74,8 +71,6 @@ class JwtHandler {
         catch(\UnexpectedValueException $e){
             return $this->_errMsg($e->getMessage());
         }
+
     }
-
 }
-
-?>
